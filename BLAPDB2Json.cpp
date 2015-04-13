bbb0ad7f6f24a904ea45eb02c00e00f1997e8625
rafaelfang <cf797@mail.missouri.edu>
2015-04-12 22:13:09 -0500

    BLAPDB2Json class

--- BLAPDB2Json.cpp
+++ BLAPDB2Json.cpp
@@ -7,99 +7,254 @@
 
 #include "BLAPDB2Json.h"
 
-BLAPDB2Json::BLAPDB2Json(char* inputFilename) :
-ConvertTools(inputFilename)  {
-	// TODO Auto-generated constructor stub
+BLAPDB2Json::BLAPDB2Json(char* inFilename, char* outFilename) :
+		ConvertTools(inFilename, outFilename) {
 
 }
 void BLAPDB2Json::convert() {
 
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	FILE* fptr = fopen(inputFilename, "r");
+	ofstream outputFile(outputJsonFilename);
 	char line[500];
 	int counter = 1;
-	FILE* inputFile = fopen(fileName, "r");
-	ofstream outputFile("BLAPDBResult.json");
-
-	outputFile << "{" << "\n";
-	while (fgets(line, 500, inputFile) != NULL) {
-
-		if (strstr(line, ">") != NULL) {
-			outputFile << "\"protein" << counter << "\":{\n";
-			//get protein name
-
-			string tmp(line);
-			outputFile << "\t\"proteinName\":\"" << tmp.substr(5, 11) << "\",\n";
-
-			while(strstr(line, "Length =") == NULL){
-				fgets(line, 500, inputFile);//skip
+	BlaPDBResult result;
+	outputFile << "{\n";
+	while (fgets(line, 500, fptr) != NULL) {
+		if ((strstr(line, ">") != NULL) && !result.isFirstStateReached()) {
+			//set first block information
+			char proteinName[7];
+			char* pos1 = strstr(line, ">");
+			sscanf(pos1 + 1, "%s", proteinName);
+			string proteinNameStr(proteinName);
+			result.setProteinName(proteinNameStr);
+			while (strstr(line, "Length =") == NULL) {
+				fgets(line, 500, fptr); //skip long name
 			}
-			//cout<<line<<endl;
-
-			//get length
 			char temp[10];
 			int seqLen;
 			sscanf(line, "%s = %d", temp, &seqLen);
-			//cout<< seqLen<<endl;
-			outputFile << "\t\"Length\":\"" << seqLen << "\",\n";
-
-			fgets(line, 500, inputFile); //blank
-
-			//get score, expect
-			fgets(line, 500, inputFile);
-
+			result.setLength(seqLen);
+			//set first state reached
+			result.setFirstStateReached(true);
+		}
+		if ((strstr(line, "Score =") != NULL)
+				&& !result.isSecondStateReached()) {
+			//set second block information
 			float score;
 			char* pos1 = strstr(line, "=");
 			sscanf(pos1 + 1, "%f", &score);
-			outputFile << "\t\"Score\":\"" << score << "\",\n";
-
+			result.setScore(score);
 			char* pos2 = strstr(pos1 + 1, "=");
 			float eValue;
 			sscanf(pos2 + 1, "%f", &eValue);
-			outputFile << "\t\"eValue\":\"" << eValue << "\",\n";
-
-			//get idenities, positives
-			fgets(line, 500, inputFile);
+			result.setExpect(eValue);
+			fgets(line, 500, fptr);
 			pos1 = strstr(line, "(");
 			int identities;
 			sscanf(pos1 + 1, "%d", &identities);
-			outputFile << "\t\"Identities\":\"" << identities << "\",\n";
-
+			result.setIdentities(identities);
 			pos2 = strstr(pos1 + 1, "(");
 			int positives;
 			sscanf(pos2 + 1, "%d", &positives);
-			outputFile << "\t\"Positives\":\"" << positives << "\",\n";
+			result.setPositives(positives);
+			char* pos3 = strstr(pos2 + 1, "(");
+			if (pos3 == NULL) {
+				//there is no gaps
+				result.setGaps(0);
+			} else {
+				int gaps;
+				sscanf(pos3 + 1, "%d", &gaps);
+				result.setGaps(gaps);
+			}
 
-			fgets(line, 500, inputFile); //blank
+			//set second state reached
+			result.setSecondStateReached(true);
+		}
 
-			//get query
+		if ((strstr(line, "Query:") != NULL) && !result.isThirdStateReached()) {
+			//set third block information
 			int nInt1, nInt2;
 			char var1[20], var2[20];
-			char tmpQuery_pro[200];
-			fgets(line, 500, inputFile);
-			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpQuery_pro, &nInt2);
-			outputFile << "\t\"QueryStart\":\"" << nInt1 << "\",\n";
-			outputFile << "\t\"Query\":\"" << tmpQuery_pro << "\",\n";
-			outputFile << "\t\"QueryEnd\":\"" << nInt2 << "\",\n";
+			char tmpQuery[200];
+			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpQuery, &nInt2);
+			result.setQueryStart(nInt1);
+			string tmpQueryStr(tmpQuery);
+			result.setQuery(tmpQueryStr);
+			result.setQueryEnd(nInt2);
+			fgets(line, 500, fptr);
+			string tmpLineStr(line);
+			result.setAlignment(tmpLineStr);
+			fgets(line, 500, fptr);
+			char tmpSubject[200];
+			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpSubject, &nInt2);
+			result.setSubjectStart(nInt1);
+			string tmpSubjectStr(tmpSubject);
+			result.setSubject(tmpSubjectStr);
+			result.setSubjectEnd(nInt2);
+			//set third state reached
+			result.setThirdStateReached(true);
+		}
 
-			//match
-			fgets(line, 500, inputFile);
-			string str(line);
-			trim(str);
-			outputFile << "\t\"alignment\":\"" << str << "\",\n";
+		if ((strstr(line, "Query:") != NULL) && result.isThirdStateReached()) {
+			//update third block information, so in this case
+			//query, alignment and subject should be long string
+			//the queryEnd and subjectEnd should also be updated
+			int nInt1, nInt2;
+			char var1[20], var2[20];
+			char tmpQuery[200];
+			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpQuery, &nInt2);
+			string newQuery = result.getQuery();
+			newQuery += tmpQuery;
+			result.setQuery(newQuery);
+			result.setQueryEnd(nInt2);
+			fgets(line, 500, fptr);
+			string newAlignment = result.getAlignment();
+			newAlignment += line;
+			result.setAlignment(newAlignment);
+			fgets(line, 500, fptr);
+			char tmpSubject[200];
+			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpSubject, &nInt2);
+			string newSubject = result.getSubject();
+			newSubject += tmpSubject;
+			result.setSubject(newSubject);
+			result.setSubjectEnd(nInt2);
 
-			//get subject
-			fgets(line, 500, inputFile);
-			sscanf(line, "%s %d %s %d", var1, &nInt1, tmpQuery_pro, &nInt2);
-			outputFile << "\t\"SubjectStart\":\"" << nInt1 << "\",\n";
-			outputFile << "\t\"Subject\":\"" << tmpQuery_pro << "\",\n";
-			outputFile << "\t\"SubjectEnd\":\"" << nInt2 << "\"\n";
+		}
 
+		if ((strstr(line, "Score =") != NULL)
+				&& result.isSecondStateReached()) {
+
+			//first write the protein information to json
+			outputFile << "\"protein" << counter << "\":{\n";
+			outputFile << "\t\"proteinName\":\"" << result.getProteinName()
+					<< "\",\n";
+			outputFile << "\t\"Length\":\"" << result.getLength() << "\",\n";
+			outputFile << "\t\"Score\":\"" << result.getScore() << "\",\n";
+			outputFile << "\t\"Expect\":\"" << result.getExpect() << "\",\n";
+			outputFile << "\t\"Identities\":\"" << result.getIdentities()
+					<< "\",\n";
+			outputFile << "\t\"Positives\":\"" << result.getPositives()
+					<< "\",\n";
+			outputFile << "\t\"Gaps\":\"" << result.getGaps() << "\",\n";
+			outputFile << "\t\"QueyStart\":\"" << result.getQueryStart()
+					<< "\",\n";
+			outputFile << "\t\"Query\":\"" << result.getQuery() << "\",\n";
+			outputFile << "\t\"QueryEnd\":\"" << result.getQueryEnd()
+					<< "\",\n";
+			outputFile << "\t\"Alignment\":\"" << result.getAlignment()
+					<< "\",\n";
+			outputFile << "\t\"SubjectStart\":\"" << result.getSubjectStart()
+					<< "\",\n";
+			outputFile << "\t\"Subject\":\"" << result.getSubject() << "\",\n";
+			outputFile << "\t\"SubjectEnd\":\"" << result.getSubjectEnd()
+					<< "\"\n";
 			outputFile << "},\n";
 			counter++;
+			//then update the information and set the second state flag
+			float score;
+			char* pos1 = strstr(line, "=");
+			sscanf(pos1 + 1, "%f", &score);
+			result.setScore(score);
+			char* pos2 = strstr(pos1 + 1, "=");
+			float eValue;
+			sscanf(pos2 + 1, "%f", &eValue);
+			result.setExpect(eValue);
+			fgets(line, 500, fptr);
+			pos1 = strstr(line, "(");
+			int identities;
+			sscanf(pos1 + 1, "%d", &identities);
+			result.setIdentities(identities);
+			pos2 = strstr(pos1 + 1, "(");
+			int positives;
+			sscanf(pos2 + 1, "%d", &positives);
+			result.setPositives(positives);
+			char* pos3 = strstr(pos2 + 1, "(");
+			if (pos3 == NULL) {
+				//there is no gaps
+				result.setGaps(0);
+			} else {
+				int gaps;
+				sscanf(pos3 + 1, "%d", &gaps);
+				result.setGaps(gaps);
+			}
+
+			result.setSecondStateReached(true);
+			//unset the third block flag
+			result.setThirdStateReached(false);
+
 		}
+		if ((strstr(line, ">") != NULL) && result.isFirstStateReached()) {
+
+			//first write the protein information to json
+			outputFile << "\"protein" << counter << "\":{\n";
+			outputFile << "\t\"proteinName\":\"" << result.getProteinName()
+					<< "\",\n";
+			outputFile << "\t\"Length\":\"" << result.getLength() << "\",\n";
+			outputFile << "\t\"Score\":\"" << result.getScore() << "\",\n";
+			outputFile << "\t\"Expect\":\"" << result.getExpect() << "\",\n";
+			outputFile << "\t\"Identities\":\"" << result.getIdentities()
+					<< "\",\n";
+			outputFile << "\t\"Positives\":\"" << result.getPositives()
+					<< "\",\n";
+			outputFile << "\t\"Gaps\":\"" << result.getGaps() << "\",\n";
+			outputFile << "\t\"QueyStart\":\"" << result.getQueryStart()
+					<< "\",\n";
+			outputFile << "\t\"Query\":\"" << result.getQuery() << "\",\n";
+			outputFile << "\t\"QueryEnd\":\"" << result.getQueryEnd()
+					<< "\",\n";
+			outputFile << "\t\"Alignment\":\"" << result.getAlignment()
+					<< "\",\n";
+			outputFile << "\t\"SubjectStart\":\"" << result.getSubjectStart()
+					<< "\",\n";
+			outputFile << "\t\"Subject\":\"" << result.getSubject() << "\",\n";
+			outputFile << "\t\"SubjectEnd\":\"" << result.getSubjectEnd()
+					<< "\"\n";
+			outputFile << "},\n";
+			counter++;
+			//then update the information set the first state flag
+			char proteinName[7];
+			char* pos1 = strstr(line, ">");
+			sscanf(pos1 + 1, "%s", proteinName);
+			string proteinNameStr(proteinName);
+			result.setProteinName(proteinNameStr);
+			while (strstr(line, "Length =") == NULL) {
+				fgets(line, 500, fptr); //skip long name
+			}
+			char temp[10];
+			int seqLen;
+			sscanf(line, "%s = %d", temp, &seqLen);
+			result.setLength(seqLen);
+			//unset second block flag and third block flag
+			result.setSecondStateReached(false);
+			result.setThirdStateReached(false);
+		}
+
 	}
+
+	//this case will print out the last protein
+	outputFile << "\"protein" << counter << "\":{\n";
+	outputFile << "\t\"proteinName\":\"" << result.getProteinName() << "\",\n";
+	outputFile << "\t\"Length\":\"" << result.getLength() << "\",\n";
+	outputFile << "\t\"Score\":\"" << result.getScore() << "\",\n";
+	outputFile << "\t\"Expect\":\"" << result.getExpect() << "\",\n";
+	outputFile << "\t\"Identities\":\"" << result.getIdentities() << "\",\n";
+	outputFile << "\t\"Positives\":\"" << result.getPositives() << "\",\n";
+	outputFile << "\t\"Gaps\":\"" << result.getGaps() << "\",\n";
+	outputFile << "\t\"QueyStart\":\"" << result.getQueryStart() << "\",\n";
+	outputFile << "\t\"Query\":\"" << result.getQuery() << "\",\n";
+	outputFile << "\t\"QueryEnd\":\"" << result.getQueryEnd() << "\",\n";
+	outputFile << "\t\"Alignment\":\"" << result.getAlignment() << "\",\n";
+	outputFile << "\t\"SubjectStart\":\"" << result.getSubjectStart()
+			<< "\",\n";
+	outputFile << "\t\"Subject\":\"" << result.getSubject() << "\",\n";
+	outputFile << "\t\"SubjectEnd\":\"" << result.getSubjectEnd() << "\"\n";
+	outputFile << "},\n";
+
+	//print the finishing line
 	outputFile << "\"finish\":\"end\"" << "\n";
 	outputFile << "}" << "\n";
-	fclose(inputFile);
+	fclose(fptr);
 	outputFile.close();
 
 }
--- BLAPDB2Json.h
+++ BLAPDB2Json.h
@@ -7,18 +7,14 @@
 
 #ifndef BLAPDB2JSON_H_
 #define BLAPDB2JSON_H_
+#include "BlaPDBResult.h"
+#include "ConvertTools.h"
 
-#include "ConvertTools.h"
-#include <stdio.h>
-#include <assert.h>
-#include <iostream>
-#include <fstream>
-#include <string.h>
-#include "ConvertTools.h"
+
 using namespace std;
 class BLAPDB2Json: public ConvertTools {
 public:
-	BLAPDB2Json(char*);
+	BLAPDB2Json(char*,char*);
 	void convert();
 	virtual ~BLAPDB2Json();
 };
