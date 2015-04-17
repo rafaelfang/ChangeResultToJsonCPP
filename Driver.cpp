3bce806c332ff487deef8f8bec56ba851ea3f348
rafaelfang <cf797@mail.missouri.edu>
2015-04-16 22:30:01 -0500

    Driver

--- Driver.cpp
+++ Driver.cpp
@@ -9,6 +9,7 @@
 #include "HHRNR2Json.h"
 #include "BLANR2Json.h"
 #include "BLAPDB2Json.h"
+#include "json/json.h"
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -16,30 +17,33 @@ int main(int argc, char* argv[]) {
 		cout << "not enough arguments" << endl;
 		cout << "<executable> <type> <inputFilename> <outputJsonFilename> "
 				<< endl;
+
 	}
+
 	// -hhr2Json query.hhr hhr.json
 	if (strcmp(argv[1], "-hhr2Json") == 0) {
+
 		HHR2Json hhr2Json(argv[2], argv[3]);
 		hhr2Json.convert();
-		hhr2Json.write2JsonFile();
+		hhr2Json.writeAlignmentResults2JsonFile();
 	}
 	// -hhrNR2Json query.hhrNR hhrNR.json
 	if (strcmp(argv[1], "-hhrNR2Json") == 0) {
 		HHRNR2Json hhrNR2Json(argv[2], argv[3]);
 		hhrNR2Json.convert();
-		hhrNR2Json.write2JsonFile();
+		hhrNR2Json.writeAlignmentResults2JsonFile();
 	}
 	// -blaNR2Json query.blaNR blaNR.json
 	if (strcmp(argv[1], "-blaNR2Json") == 0) {
 		BLANR2Json blaNR2Json(argv[2], argv[3]);
 		blaNR2Json.convert();
-		blaNR2Json.write2JsonFile();
+		blaNR2Json.writeAlignmentResults2JsonFile();
 	}
 	// -blaPDB2Json query.blaPDB blaPDB.json
 	if (strcmp(argv[1], "-blaPDB2Json") == 0) {
 		BLAPDB2Json blaPDB2Json(argv[2], argv[3]);
 		blaPDB2Json.convert();
-		blaPDB2Json.write2JsonFile();
+		blaPDB2Json.writeAlignmentResults2JsonFile();
 	}
 
 }
